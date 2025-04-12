0bbbe18dabb4dbcebbd653d32f47855461bd9ac7
Konstantinos Mokos <konstantinosmokos157@gmail.com>
2025-04-12 04:44:21 -0400

    users fix

--- server/models/users.ts
+++ server/models/users.ts
@@ -1,5 +1,4 @@
 import {users} from '../config/mongoCollections.js';
-import {ObjectId} from 'mongodb';
 import bcrypt from 'bcrypt';
 import {checkString, checkStringTrimmed} from '../typechecker.js';
 
