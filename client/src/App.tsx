3481efc25d012b2bf9699d950f2fc644836b5bb7
Weiting Kuo <kuowesley1997@gmail.com>
2025-04-17 20:30:43 -0400

    Update client, Add Navbar and Home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, useLocation } from "react-router-dom";
 import './App.css'
 
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation();
+
+  const hideNavRoutes = ["/login", "/register"];
+  const shouldHideNav = hideNavRoutes.includes(location.pathname);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header>
+        {!shouldHideNav && <Navbar />}
+      </header>
+      <div className="App">
+          <Routes>
+              <Route path="/" element={<Home />} />
+          </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import reactLogo from '../../assets/react.svg'
+import "./Navbar.css";
+
+// Navbar items type
+interface NavItemType {
+  label: string;
+  href: string;
+  icon?: React.ReactNode;
+}
+
+
+const navItems: NavItemType[] = [
+  { label: "Home", href: "/" },
+  { label: "Page1", href: "/" },
+  { label: "Page2", href: "/" },
+];
+
+// Navbar item
+const NavItem: React.FC<{ item: NavItemType }> = ({ item }) => (
+  <NavLink
+    to={item.href}
+    className={({ isActive }) =>
+      `nav-item ${isActive ? "nav-item-active" : ""}`
+    }
+  >
+    {item.label}
+  </NavLink>
+);
+
+// Navbar 
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="left-section">
+          {/* Logo */}
+          <img src={reactLogo} alt="React Logo" className="logo" />
+          <div className="nav-items">
+            {navItems.map((item, index) => (
+              <NavItem key={index} item={item} />
+            ))}
+          </div>
+        </div>
+
+
+        <div className="right-section">
+          
+          <input
+            type="text"
+            placeholder="Search..."
+            className="search-input"
+          />
+          <div className="auth-buttons">
+            <Link to="/login">Login</Link>
+            {" "}
+            <Link to="/register">Register</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
