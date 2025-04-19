784a050e70d2700106d0ba3e0356ef84d2a704b3
Weiting Kuo <kuowesley1997@gmail.com>
2025-04-19 00:03:02 -0400

    add cors in server

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from "express";
 import session from "express-session";
 import configRoutes from "./routes/index"
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 app.get("/ping", (req: Request, res: Response) => {
   res.send("pong");
