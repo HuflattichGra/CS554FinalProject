45cbbe1874f97d2870ceaf8bf617eb6a66cfa3d2
ChenHaolinOlym <51154093+ChenHaolinOlym@users.noreply.github.com>
2025-05-09 16:51:28 -0400

    Update post create route to be able to add multiple images

--- server/src/posts.ts
+++ server/src/posts.ts
@@ -1,157 +1,201 @@
-import { ObjectId } from 'mongodb';
-import { posts } from '../config/mongoCollections';
-import * as typecheck from '../typechecker.js';
+import { ObjectId } from "mongodb";
+import { posts } from "../config/mongoCollections";
+// @ts-ignore
+import * as typecheck from "../typechecker.js";
 
 export type Post = {
-    _id: string;
-    conventionID: string;
-    userID: string;
-    text: string;
-    images: Array<string>;
-    likes: Array<string>;
-    createdAt: Date;
+  _id: string;
+  conventionID: string;
+  userID: string;
+  text: string;
+  images: Array<string>;
+  likes: Array<string>;
+  createdAt: Date;
 };
 
-const checkIDS = (x: any,var_name:string) => {
-    typecheck.checkId(x,var_name);
-}
+const checkIDS = (x: any, var_name: string) => {
+  typecheck.checkId(x, var_name);
+};
 
 function DEBUG_generatePost() {
-    var obj: Post = {
-        _id: (new ObjectId()).toHexString(),
-        conventionID: (new ObjectId().toHexString()),
-        userID: (new ObjectId()).toHexString(),
-        text: "This is a sample post",
-        images: [],
-        likes: [],
-        createdAt: new Date(Date.now())
-    }
-    return obj;
+  var obj: Post = {
+    _id: new ObjectId().toHexString(),
+    conventionID: new ObjectId().toHexString(),
+    userID: new ObjectId().toHexString(),
+    text: "This is a sample post",
+    images: [],
+    likes: [],
+    createdAt: new Date(Date.now()),
+  };
+  return obj;
 }
 
-function checkPost(obj: any, needsID: boolean = false, noEmpty: boolean = true) {
-    var postObj: Post = obj;
-    if (obj._id != undefined || needsID) { typecheck.checkId(obj._id, "post.id"); }
-    if (obj.conventionID != undefined || noEmpty) { if(obj.conventionID.length > 0){ typecheck.checkId(obj.conventionID, "post.conventionID"); } }
-    if (obj.userID != undefined || noEmpty) { typecheck.checkId(obj.userID, "post.userID"); }
-    if (obj.text != undefined || noEmpty) { postObj.text = typecheck.checkStringTrimmed(obj.text, "post.text"); }
-    if (obj.images != undefined || noEmpty) { if(obj.images.length > 0){obj.images.map(checkIDS, "post.images"); }}
-    if (obj.likes != undefined || noEmpty) { if(obj.likes.length > 0){obj.likes.map(checkIDS, "post.likes"); }}
-    return postObj;
+function checkPost(
+  obj: any,
+  needsID: boolean = false,
+  noEmpty: boolean = true
+) {
+  var postObj: Post = obj;
+  if (obj._id != undefined || needsID) {
+    typecheck.checkId(obj._id, "post.id");
+  }
+  if (obj.conventionID != undefined || noEmpty) {
+    if (obj.conventionID.length > 0) {
+      typecheck.checkId(obj.conventionID, "post.conventionID");
+    }
+  }
+  if (obj.userID != undefined || noEmpty) {
+    typecheck.checkId(obj.userID, "post.userID");
+  }
+  if (obj.text != undefined || noEmpty) {
+    postObj.text = typecheck.checkStringTrimmed(obj.text, "post.text");
+  }
+  if (obj.images != undefined || noEmpty) {
+    if (obj.images.length > 0) {
+      obj.images.map(checkIDS, "post.images");
+    }
+  }
+  if (obj.likes != undefined || noEmpty) {
+    if (obj.likes.length > 0) {
+      obj.likes.map(checkIDS, "post.likes");
+    }
+  }
+  return postObj;
 }
 
 async function addPost(obj: any) {
-    obj = checkPost(obj, false, true);
+  obj = checkPost(obj, false, true);
 
-    obj.userID = ObjectId.createFromHexString(obj.userID);
-    if(obj.conventionID){
-        obj.conventionID = ObjectId.createFromHexString(obj.conventionID);
-    }
+  obj.userID = ObjectId.createFromHexString(obj.userID);
+  if (obj.conventionID) {
+    obj.conventionID = ObjectId.createFromHexString(obj.conventionID);
+  }
 
-    obj.createdAt = new Date(Date.now());
+  obj.createdAt = new Date(Date.now());
 
-    const db = await posts();
-    var dbOut = await db.insertOne(obj);
-    if (dbOut == null) {
-        throw new Error("Post was not found in DB");
-    }
+  const db = await posts();
+  var dbOut = await db.insertOne(obj);
+  if (dbOut == null) {
+    throw new Error("Post was not found in DB");
+  }
 
-    var id: string = (dbOut.insertedId.toHexString());
+  var id: string = dbOut.insertedId.toHexString();
 
-    var retVal: Post = await getPost(id);
+  var retVal: Post = await getPost(id);
 
-    return retVal;
+  return retVal;
 }
 
 async function getPost(id: string) {
-    typecheck.checkId(id);
+  typecheck.checkId(id);
 
-    const db = await posts();
-    var retVal: Post = await db.findOne({ _id: ObjectId.createFromHexString(id) });
+  const db = await posts();
+  var retVal: Post = await db.findOne({
+    _id: ObjectId.createFromHexString(id),
+  });
 
-    if (retVal == null) {
-        throw new Error("Post with id " + id + " not found");
-    }
+  if (retVal == null) {
+    throw new Error("Post with id " + id + " not found");
+  }
 
-    return retVal;
+  return retVal;
 }
 
 async function getPosts() {
-    const db = await posts();
-    var retVal: Array<Post> = await db.find({}).toArray();
+  const db = await posts();
+  var retVal: Array<Post> = await db.find({}).toArray();
 
-    if (retVal == null) {
-        throw new Error("No posts are available");
-    }
+  if (retVal == null) {
+    throw new Error("No posts are available");
+  }
 
-    retVal.sort(sortByDate);
+  retVal.sort(sortByDate);
 
-    return retVal;
+  return retVal;
 }
 
 async function updatePost(id: string, obj: any) {
-    typecheck.checkId(id);
-    checkPost(obj, false, false);
-    delete obj._id;
+  typecheck.checkId(id);
+  checkPost(obj, false, false);
+  delete obj._id;
 
-    const db = await posts();
-    var updateRes: Post = await db.updateOne({ _id: ObjectId.createFromHexString(id) }, [{ $set: obj }]);
+  const db = await posts();
+  var updateRes: Post = await db.updateOne(
+    { _id: ObjectId.createFromHexString(id) },
+    [{ $set: obj }]
+  );
 
-    if (updateRes == null) {
-        throw new Error("No posts are available");
-    }
+  if (updateRes == null) {
+    throw new Error("No posts are available");
+  }
 
-    var retVal = await getPost(id);
+  var retVal = await getPost(id);
 
-    return retVal;
+  return retVal;
 }
 
 async function deletePost(id: string) {
-    typecheck.checkId(id,"id");
+  typecheck.checkId(id, "id");
 
-    const db = await posts();
-    var retVal: Post = await getPost(id);
-    var deleteRes: Post = await db.deleteOne({ _id: ObjectId.createFromHexString(id) });
+  const db = await posts();
+  var retVal: Post = await getPost(id);
+  var deleteRes: Post = await db.deleteOne({
+    _id: ObjectId.createFromHexString(id),
+  });
 
-    if (deleteRes == null) {
-        throw new Error("delete of " + deleteRes + "failed");
-    }
+  if (deleteRes == null) {
+    throw new Error("delete of " + deleteRes + "failed");
+  }
 
-    return retVal;
+  return retVal;
 }
 
-async function getPostsByUserId(id: string) { 
-    typecheck.checkId(id);
+async function getPostsByUserId(id: string) {
+  typecheck.checkId(id);
 
-    const db = await posts();
-    var retVal: Array<Post> = await db.find({userID: ObjectId.createFromHexString(id)}).toArray();
+  const db = await posts();
+  var retVal: Array<Post> = await db
+    .find({ userID: ObjectId.createFromHexString(id) })
+    .toArray();
 
-    if (retVal == null) {
-        throw new Error("No posts are available");
-    }
+  if (retVal == null) {
+    throw new Error("No posts are available");
+  }
 
-    retVal.sort(sortByDate);
+  retVal.sort(sortByDate);
 
-    return retVal;
+  return retVal;
 }
 
-async function getPostsByConventionId(id:string){
-    typecheck.checkId(id);
+async function getPostsByConventionId(id: string) {
+  typecheck.checkId(id);
 
-    const db = await posts();
-    var retVal: Array<Post> = await db.find({conventionID: ObjectId.createFromHexString(id)}).toArray();
+  const db = await posts();
+  var retVal: Array<Post> = await db
+    .find({ conventionID: ObjectId.createFromHexString(id) })
+    .toArray();
 
-    if (retVal == null) {
-        throw new Error("No posts are available");
-    }
+  if (retVal == null) {
+    throw new Error("No posts are available");
+  }
 
-    retVal.sort(sortByDate);
+  retVal.sort(sortByDate);
 
-    return retVal;
+  return retVal;
 }
 
-function sortByDate(a:Post,b:Post){
-    return a.createdAt > b.createdAt ? -1 : ((a.createdAt < b.createdAt) ? 1 : 0);
+function sortByDate(a: Post, b: Post) {
+  return a.createdAt > b.createdAt ? -1 : a.createdAt < b.createdAt ? 1 : 0;
 }
 
-export default { checkPost, addPost, getPosts, getPost, updatePost, deletePost, DEBUG_generatePost, getPostsByUserId,getPostsByConventionId };
+export default {
+  checkPost,
+  addPost,
+  getPosts,
+  getPost,
+  updatePost,
+  deletePost,
+  DEBUG_generatePost,
+  getPostsByUserId,
+  getPostsByConventionId,
+};
