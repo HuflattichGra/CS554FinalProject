4b4f65ea70b8c3089648c7e50c71c8340d1ccd61
ChenHaolinOlym <51154093+ChenHaolinOlym@users.noreply.github.com>
2025-05-13 18:22:43 -0400

    Fix docker network error

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,3 +1,4 @@
-export const API_BASE = import.meta.env.DOCKER
-  ? "/api"
-  : import.meta.env.VITE_API_URL || "http://localhost:3000";
+export const API_BASE =
+  import.meta.env.VITE_DOCKER === "true"
+    ? "/api"
+    : import.meta.env.VITE_API_URL || "http://localhost:3000";
