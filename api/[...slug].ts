13dfda59363510b223128c08780ebc9479643692
HuflattichGra <894032952@qq.com>
2025-10-03 01:08:55 -0400

    Add debug logging to slug handler

--- api/[...slug].ts
+++ api/[...slug].ts
@@ -1,3 +1,10 @@
 import app from '../server/vercel.js';
 
-export default app;
+// Debug wrapper to understand what's happening
+export default function handler(req: any, res: any) {
+  console.log('API slug handler called:', req.url, req.method);
+  console.log('Query params:', req.query);
+  
+  // Call the Express app
+  return app(req, res);
+}
